abe9ba7457e5ac04060599e472e3c715a682c1f4
Bruno Silva <brunosilv_a@outlook.com>
2018-04-22 00:53:47 -0300

    Corrigindo exercicios com alunos

--- estruturas-repeticao/codigos/q6.py
+++ estruturas-repeticao/codigos/q6.py
@@ -1,10 +1,23 @@
-termo = 0
-while (termo <= 0):
-    termo = int(input("Voce quer o fatorial de qual termo: "))
-    if (termo <= 0):
-        print ("O termo deve ser positivo e diferente de 0!")
+termo = int(input("\nVoce quer o fatorial de qual termo: "))
 
-fatorial = 1
-for i in range(1, termo + 1):
-    fatorial *= i
-    print (fatorial)
+while (termo < 0):
+    print ("O termo deve ser positivo!")
+    termo = int(input("\nVoce quer o fatorial de qual termo: "))        
+
+if (termo == 0):
+    print("Fatorial de 0 é 1")
+
+else:
+    fatorial = 1
+    print("\nCom for!")
+    for i in range(1, termo + 1):
+        fatorial *= i
+        print (fatorial)
+    
+    fatorial = 1
+    contador = 1
+    print("\nCom while!")
+    while(contador <= termo):
+        fatorial *= contador
+        print (fatorial)
+        contador += 1    
