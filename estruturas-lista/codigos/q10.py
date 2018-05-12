78df99857530b39fdc8991e838c6b24ce14b0c4b
Bruno Silva <brunosilv_a@outlook.com>
2018-05-12 03:17:06 -0300

    Questões 7 - 10 [alp - estruturas-listas]

---
+++ estruturas-lista/codigos/q10.py
@@ -0,0 +1,9 @@
+palavra = "bruno borges"
+sumvog = 0
+
+for i in range(len(palavra)):
+    if(palavra[i] in ('a','e','i','o','u')):
+        sumvog += 1
+
+print("\nNumero de vogais:", sumvog)
+print("Numero de consoantes: ", len(palavra) - sumvog)
