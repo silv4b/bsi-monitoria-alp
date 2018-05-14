f9fb112811f0327decaa3b589856beaa92be7098
Bruno Silva <brunosilv_a@outlook.com>
2018-05-14 16:47:29 -0300

    ocultar respostas da lista de listasALP

--- estruturas-lista/codigos/q10.py
+++
@@ -1,9 +0,0 @@
-palavra = input("Palavra: ")
-sumvog = 0
-
-for i in range(len(palavra)):
-    if(palavra[i] in ('a','e','i','o','u')):
-        sumvog += 1
-
-print("\nNumero de vogais:", sumvog)
-print("Numero de consoantes: ", len(palavra) - sumvog)
